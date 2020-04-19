69bf9c275c67ca46a585769b7612c081d18352ea
Nathan Aguiar <nathanaguiarbs@gmail.com>
2020-04-19 17:21:35 -0300

    get post by id

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.codeblog.model.Post;
@@ -24,4 +25,15 @@ public class CodeblogController {
 		
 		return mv;
 	}
+	@GetMapping("/posts/{id}")
+	public ModelAndView getPostById(@PathVariable("id") Long id) {
+		
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = codeblogService.findById(id);
+		mv.addObject("post", post);
+		
+		return mv;
+	}
+	
+	
 }
