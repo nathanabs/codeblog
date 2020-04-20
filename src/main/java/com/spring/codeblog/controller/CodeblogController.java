07105c3a8f36f20274ea22dcecfeaba071ac5d29
Nathan Aguiar <nathanaguiarbs@gmail.com>
2020-04-20 00:14:36 -0300

    new post and spring security

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -1,12 +1,18 @@
 package com.spring.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
@@ -35,5 +41,25 @@ public class CodeblogController {
 		return mv;
 	}
 	
+	@GetMapping("/newPost")
+	public String getPostForm() {
+		
+		return "postForm";
+	}
 	
+	@PostMapping("/newPost")
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		
+		attributes.addFlashAttribute("message", "Verifique se os campos obrigatórios foram preenchidos.");
+		
+		if(result.hasErrors()) {
+			return "redirect:/newPost";
+		}
+		
+		post.setDate(LocalDate.now());
+		
+		codeblogService.save(post);
+		
+		return "redirect:/posts";
+	}
 }
